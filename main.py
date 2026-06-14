709fba2a0bddfe0fbec6a4d25bd7ae7d36a55f84
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-14 23:38:57 +0300

    Project completion (first version)

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter,FastAPI
+from logs.logger_app import logger
+from database.db_connection import db
+from routes.book_routes import router as book_routes
+from routes.member_routes import router as member_routes
+from routes.report_routes import router as report_routes
+from contextlib import asynccontextmanager
+
+
+
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    db.init_db
+    db.init_tables
+    db._connection
+
+    print("welcome")
+    logger.info("Connection to the database has been established.")
+
+    yield
+
+    db._connection.close()
+    print("shot down")
+    logger.info("Data connection closed, system is shutting down.")
+    db._connection.close()
+
+
+
+
+
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+app.include_router(book_routes,prefix="/books",tags=["books"])
+
+app.include_router(member_routes,prefix="/members",tags=["members"])
+
+app.include_router(report_routes,prefix="/reports",tags=["reports"])
+
+    
+
+
+
+
